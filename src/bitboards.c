4ab56834605207acc926be1e047dd9d4fc5e6ab0
hoseony <hoseon.alex.yu@gmail.com>
2026-03-29 01:55:07 -0400

    fixed naming, made move gen faster with popLSB

--- src/bitboards.c
+++ src/bitboards.c
@@ -7,38 +7,56 @@
 #include "types.h" 
 
 //-------- function prototype ----------
-void put_bit(U64 *board, Board_pos pos);
-void remove_bit(U64 *board, Board_pos pos);
-void move_bit(U64 *board, Board_pos start_pos, Board_pos target_pos);
-// so you are inputing board as &board type
-void print_bb(U64 board);
-int index_shift(int, int); 
+void putBit(U64 *board, Board_pos pos);
+void removeBit(U64 *board, Board_pos pos);
+void moveBit(U64 *board, Board_pos start_pos, Board_pos target_pos);
+int indexShift(int rank, int file); 
+int popLSB(U64 *board);
+
+void printBitboard(U64 board);
+State initializeState();
+U64 blackOccupied(State p);
+U64 whiteOccupied(State p);
+U64 allOccupied(State p);
 
 //------- Simple Bit Manipulation ---------
 
-inline void put_bit(U64 *board, Board_pos pos) {
+inline void putBit(U64 *board, Board_pos pos) {
     *board |= (1ULL << pos);
 }
 
-inline void remove_bit(U64 *board, Board_pos pos) {
+inline void removeBit(U64 *board, Board_pos pos) {
     *board &= ~(1ULL << pos);
 }
 
-inline void move_bit(U64 *board, Board_pos from, Board_pos to) {
+inline void moveBit(U64 *board, Board_pos from, Board_pos to) {
     *board &= ~(1ULL << from); 
-    put_bit(board, to);
+    putBit(board, to);
 }
 
-inline int index_shift(int r, int f) {
+inline int indexShift(int r, int f) {
     return f + r * 8; 
 }
 
+int popLSB(U64 *board) {
+    // this is a funny operation from gnu compiler that gets the LSB index
+    // if you are interested in, check gnu documentation
+    // https://gcc.gnu.org/onlinedocs/gcc/Bit-Operation-Builtins.html 
+    int square = __builtin_ctzll(*board);
+
+    // some DLD type thing
+    *board &= *board - 1;
+    return square;
+}
+
 // ------------- Print Board ----------------
-void print_bb(U64 board) {
+// it prints the bit board
+void printBitboard(U64 board) {
     printf("---------- bitboard ---------\n\n");
 
     for (int rank = 7; rank >= 0; rank--) {
         printf("  %1d   ", rank + 1);
+
         for (int file = 0; file < 8; file++) {
             U64 square = rank * 8 + file;
             int number = ((1ULL << square) & board)? 1 : 0;
@@ -46,18 +64,17 @@ void print_bb(U64 board) {
         }
         printf("\n");
     }
-
     printf("\n      ");
 
     for (int file = 0; file < 8; file ++) {
         printf("%2c ", 'a' + file);
     }
-
     printf("\n\n");
 }
 
 // --------- Initialize Board ------------
-State initialize_state() {
+// This initialize the starting position of the board;
+State initializeState() {
     State p;
     p.wp = 0x000000000000FF00;
     p.wn = 0x0000000000000042;
@@ -75,132 +92,99 @@ State initialize_state() {
     p.turn = WHITE;
     return p;
 }
+
 // ---------Combined Board ------------
-U64 black_occ(State *p) {
-    return (p->bp | p->bn | p->bb | p->br | p->bq | p->bk);
+// These make occupied bitboards
+U64 blackOccupied(State p) {
+    return (p.bp | p.bn | p.bb | p.br | p.bq | p.bk);
 }
 
-U64 white_occ(State *p) {
-    return (p->wp | p->wn | p->wb | p->wr | p->wq | p->wk);
+U64 whiteOccupied(State p) {
+    return (p.wp | p.wn | p.wb | p.wr | p.wq | p.wk);
 }
 
-U64 all_occ(State *p) {
-    return (p->bp | p->bn | p->bb | p->br | p->bq | p->bk | p->wp | p->wn | p->wb | p->wr | p->wq | p->wk);
+U64 allOccupied(State p) {
+    return (p.bp | p.bn | p.bb | p.br | p.bq | p.bk | p.wp | p.wn | p.wb | p.wr | p.wq | p.wk);
 }
 
 // --------- move gen ----------
 
-// *MG for move generation
-// ** also we should be able to like check move for one specific piece
-//
-// I ma not sure yet if we should make this for each piece or integrate this on our MG functions
-// you can do this like this:
-//
-// U64 knights = p.wn
-// while (knights) {
-//      U64 oneKnight = knights & (-knights);
-//      U64 moves = MG_knights(oneKnight);
-//      remove that bit;
-// }
-
-
 // Could be more efficient
-U64 MG_knight(U64 board) {
-    U64 MG_knight = 0;
-    U64 shifter = 0; 
+// There should be better way of doing this
+U64 generateKnightMove(int square) {
+    U64 knightMove = 0;
+    U64 shifter = (1ULL << square); 
 
-    for(int i = 0; i < 64; i++) {
-        if ( ((shifter = (1ULL << i)) | board) == board )  {
-            // Generates moves for knight dependent on row and column 
-            // (does not add invalid moves if knight is on edge of board) 
+    // does not add invalid moves if knight is on edge of board 
 
-            MG_knight |= (shifter << 17) * !(((BIT_8_RANK | BIT_7_RANK) & shifter) || (BIT_H_FILE & shifter)); 
-            MG_knight |= (shifter << 15) * !(((BIT_8_RANK | BIT_7_RANK) & shifter) || (BIT_A_FILE & (shifter))); 
-            MG_knight |= (shifter << 6) * !((BIT_8_RANK & shifter) || ((BIT_A_FILE | BIT_B_FILE) & (shifter))); 
-            MG_knight |= (shifter << 10) * !((BIT_8_RANK & shifter) || ((BIT_G_FILE | BIT_H_FILE) & (shifter)));
-            MG_knight |= (shifter >> 17) * !(((BIT_1_RANK | BIT_2_RANK) & shifter) || (BIT_A_FILE & (shifter)));
-            MG_knight |= (shifter >> 15) * !(((BIT_1_RANK | BIT_2_RANK) & shifter) || (BIT_H_FILE & (shifter))); 
-            MG_knight |= (shifter >> 6) * !(((BIT_1_RANK & shifter) || ((BIT_G_FILE | BIT_H_FILE) & (shifter)))); 
-            MG_knight |= (shifter >> 10) * !((BIT_1_RANK & shifter) || ((BIT_A_FILE | BIT_B_FILE) & (shifter))); 
-        }
-    }
-    return MG_knight;
+    knightMove |= (shifter << 17) * !(((BIT_8_RANK | BIT_7_RANK) & shifter) || (BIT_H_FILE & shifter)); 
+    knightMove |= (shifter << 15) * !(((BIT_8_RANK | BIT_7_RANK) & shifter) || (BIT_A_FILE & (shifter))); 
+    knightMove |= (shifter << 6) * !((BIT_8_RANK & shifter) || ((BIT_A_FILE | BIT_B_FILE) & (shifter))); 
+    knightMove |= (shifter << 10) * !((BIT_8_RANK & shifter) || ((BIT_G_FILE | BIT_H_FILE) & (shifter)));
+    knightMove |= (shifter >> 17) * !(((BIT_1_RANK | BIT_2_RANK) & shifter) || (BIT_A_FILE & (shifter)));
+    knightMove |= (shifter >> 15) * !(((BIT_1_RANK | BIT_2_RANK) & shifter) || (BIT_H_FILE & (shifter))); 
+    knightMove |= (shifter >> 6) * !(((BIT_1_RANK & shifter) || ((BIT_G_FILE | BIT_H_FILE) & (shifter)))); 
+    knightMove |= (shifter >> 10) * !((BIT_1_RANK & shifter) || ((BIT_A_FILE | BIT_B_FILE) & (shifter))); 
+
+    return knightMove;
 }
 
-U64 MG_rook(U64 board) {
-    U64 MG_rook = 0;
-    U64 shifter = 0;
-    for(int i = 0; i < 64; i++) {
-        if( ((shifter = (1ULL << i)) | board) == board ) {
-            MG_rook |= (BIT_A_FILE << (i % 8));
-            MG_rook |= (BIT_1_RANK << (i / 8) * 8);
-        }
-    }
-
-    MG_rook &= ~board;
-    return MG_rook;
-}
-
-// (r * 8 + f) can be replaced to index_shift function
-// But idk how much that helps in terms of readability of the code
-U64 MG_bishop(U64 board) {
-    U64 MG_bishop = 0;
-
+U64 generateRookMove(int square) {
+    U64 rookMove = 0;
     int r, f;
 
-    for (int i = 0; i < 64; i ++) {
-        if( ((1ULL << i) | board) == board ) {
-            int file = i % 8;
-            int rank = i / 8;
+    int file = square % 8;
+    int rank = square / 8;
 
-            for (r = rank + 1, f = file + 1; f < 8 && r < 8; r++, f++) {
-                MG_bishop |= (1ULL << (r * 8 + f));
-            }
+    for (r = rank + 1; r < 8; r++) rookMove |= (1ULL << (r * 8 + file));
+    for (r = rank - 1; r >= 0; r--) rookMove |= (1ULL << (r * 8 + file));
+    for (f = file + 1; f < 8; f++) rookMove |= (1ULL << (rank * 8 + f));
+    for (f = file - 1; f >= 0; f--) rookMove |= (1ULL << (rank * 8 + f));
 
-            for (r = rank + 1, f = file - 1; f >= 0 && r < 8; r++, f--) {
-                MG_bishop |= (1ULL << (r * 8 + f));
-            }
-
-            for (r = rank - 1, f = file + 1; f < 8 && r >= 0; r--, f++) {
-                MG_bishop |= (1ULL << (r * 8 + f));
-            }
-
-            for (r = rank - 1, f = file - 1; f >= 0 && r >= 0; r--, f--) {
-                MG_bishop |= (1ULL << (r * 8 + f));
-            }
-        }
-    }
-    return MG_bishop;
+    return rookMove;
 }
 
-U64 MG_king(U64 board) {
+U64 generateBishopMove(int square) {
+    U64 bishopMove = 0;
+    int r, f;
+
+    int file = square % 8;
+    int rank = square / 8;
+
+    for (r = rank + 1, f = file + 1; f < 8 && r < 8; r++, f++) bishopMove |= (1ULL << (r * 8 + f));
+    for (r = rank + 1, f = file - 1; f >= 0 && r < 8; r++, f--) bishopMove |= (1ULL << (r * 8 + f));
+    for (r = rank - 1, f = file + 1; f < 8 && r >= 0; r--, f++) bishopMove |= (1ULL << (r * 8 + f));
+    for (r = rank - 1, f = file - 1; f >= 0 && r >= 0; r--, f--) bishopMove |= (1ULL << (r * 8 + f));
+
+    return bishopMove;
+}
+
+U64 generateKingMove(int square) {
     
 
 }
 
-U64 MG_queen(U64 board) {
-    return (MG_rook(board) | MG_bishop(board));
+U64 generateQueenMove(int square) {
+    return (generateBishopMove(square) | generateRookMove(square));
 }
 
-U64 MG_wpawn(U64 board, Move lastmove) {
+U64 generateWhitePawnMove(U64 board, Move lastmove) {
     //U64 black_occ = black_occ(board);
 
-
-
     // capture
     // en-passan
     // direction
     // two move initial
 }
 
-U64 MG_bpawn(U64 board) {
+U64 generateBlackPawnMove(U64 board) {
     // capture
     // en-passan
     // direction
     // two move initial
 }
 
-U64 pawn_promotion(U64 board) {
+U64 pawnPromotion(U64 board) {
     // I think this can be combined because white pawn 
     // cannot go to the rank 1 and black pawn cannot go to the 
     // rank 8 
@@ -210,7 +194,7 @@ U64 pawn_promotion(U64 board) {
 
 // -------------------------------
 
-void print_game_board(State p) {
+void printGameBoard(State p) {
     for(int i = 0; i < 64; i++) {
         if (p.bp & (1ULL << i)) printf("♟︎"); 
         else if (p.bn & (1ULL << i)) printf("♞");
@@ -236,19 +220,24 @@ void print_game_board(State p) {
 
 // ------------------------------
 int main() {
-    State p = initialize_state();
+    State p = initializeState();
 
     U64 testBit1 = 0x0000000800000000;
     U64 testBit2 = 0x1000000000000000;
 
-    MG_rook(testBit1);
-    MG_bishop(testBit1);
+    U64 temp = testBit1;
+    int square = popLSB(&temp);
 
-    U64 MGQueen = MG_queen(testBit1);
-    print_bb(testBit1);
-    print_bb(MGQueen);
+    printBitboard(generateRookMove(square));
+    printBitboard(generateBishopMove(square));
+    printBitboard(generateQueenMove(square));
 
-    print_game_board(p);
+    // so the general idea will be 
+    // U64 allMoves = 0;
+    // while (temp)
+    //      square = popLSB(temp)
+    //      allMoves |= generateRookMove(square)
+    // This will be computationally faster than the way we were doing previously
     return 0;
 }
 
