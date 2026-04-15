cf7f43fe678a6125c9d2a9e95a6401cf42f71431
hoseony <hoseon.alex.yu@gmail.com>
2026-04-15 19:18:21 -0400

    readme + magic file (nothing happened there tho)

--- README.md
+++ README.md
@@ -2,4 +2,28 @@
 
 The goal of this project is to make a working bitboard based chess engine in C.
 
-chess pieces png: https://greenchess.net/info.php?item=downloads
+## Acknowledgement
+### Assets
+- [chess pieces png](https://greenchess.net/info.php?item=downloads)
+
+### Videos / Articles
+This project would not been possible without these sources. Thank you for the authors, and I hope you find it useful and interesting! 
+
+The main inspiration for this project was from Sebastian Lague's Chess Engine series.
+- [Coding Adventure: Chess](https://youtu.be/U4ogK0MIzqk?si=00jx32x_qVhYGBIi) 
+- [Coding Adventure: Making a Better Chess Bot](https://youtu.be/_vqlIPDR2TU?si=2ZRoP0sTJO0hp0LV)
+- [I ran a Chess Programming Tournament, Here's How it Went!](https://youtu.be/Ne40a5LkK6A?si=4vlCCHpiFXIOF8ut)
+
+These are all the sources that we found helpful:
+- [Chess Programming Wiki](https://www.chessprogramming.org/Main_Page)
+- [Engine Programming (Discord Server)](https://discord.com/invite/F6W6mMsTGN)
+- [Magical Bitboards and How to Find Them: Sliding move generation in chess](https://analog-hors.github.io/site/magic-bitboards/)
+- [UCI Protocol (We do not plan to implement this at the current stage)](https://backscattering.de/chess/uci/)
+- [Algorithms Explained – minimax and alpha-beta pruning](https://www.youtube.com/watch?v=l-hh51ncgDI)
+- [DogeyStamp](https://www.dogeystamp.com/index.html)
+- [Forsyth-Edwards Notation (FEN)](https://www.chess.com/terms/fen-chess)
+- 
+
+Here are some articles we found recently that we hoped we knew it:
+- [Writing a chess engine in C++](https://ameye.dev/notes/chess-engine/)
+
--- src/main.c
+++ src/main.c
@@ -13,22 +13,103 @@ State currentState;
 State prevState;
 State prevprevState; 
 
+static int king[64] = {
+    -80, -80, -80, -80, -80, -80, -80, -80,
+    -80, -80, -80, -80, -80, -80, -80, -80,
+    -80, -80, -80, -80, -80, -80, -80, -80,
+    -80, -80, -80, -80, -80, -80, -80, -80,
+    -60, -60, -60, -60, -60, -60, -60, -60,
+    -40, -40, -40, -40, -40, -40, -40, -40,
+    -20, -20, -20, -20, -20, -20, -20, -20,
+     20,  30,  10, -30, -10, -20,  30,  20,
+};
+
+static int pawn[64] = {
+    00, 00, 00,  00,  00, 00, 00, 00,
+    00, 00, 00,  00,  00, 00, 00, 00,
+    00, 00, 00,  10,  10, 00, 00, 00,
+    00, 00, 10,  20,  20, 10, 00, 00,
+    00, 00, 10,  20,  20, 10, 00, 00,
+    05, 00, 00,  10,  10, 00, 00, 05,
+    00, 00, 00, -15, -15, 00, 00, 00,
+    00, 00, 00,  00,  00, 00, 00, 00,
+};
+
+static int knight[64] = {
+    -10,   10,  -10, -10, -10,  -10, -10,  -10,
+    -10,   00,   00,  00,  00,   00,  00,  -10,
+    -10,   00,   10,  15,  15,   10,  00,  -10,
+    -10,   00,   15,  20,  20,   15,  00,  -10,
+    -10,   00,   15,  20,  20,   15,  00,  -10,
+    -10,   00,   25,  15,  15,   25,  00,  -10,
+    -10,   00,   00,  00,  00,   00,  00,  -10,
+    -10,  -30,  -20,  00,  00,  -20, -30,  -10,
+};
+
+static int bishop[64] = {
+    -10, -10, -10, -10, -10, -10, -10, -10,
+    -10,  00,  10,  10,  10,  10,  00, -10,
+    -10,  10,  15,  15,  15,  15,  10, -10,
+    -10,  10,  15,  20,  20,  15,  10, -10,
+    -10,  10,  15,  25,  25,  15,  10, -10,
+    -10,  10,  15,  15,  15,  15,  10, -10,
+    -10,  00,  10,  10,  10,  10,  00, -10,
+    -10, -10, -30, -10, -10, -30, -10, -10,
+};
+
+static int rook[64] = {
+     00, 00, 00, 00, 00, 00, 00,  00,
+    -10, 10, 10, 10, 10, 10, 10, -10,
+    -10, 00, 00, 00, 00, 00, 00, -10,
+    -10, 00, 00, 00, 00, 00, 00, -10,
+    -10, 00, 00, 00, 00, 00, 00, -10,
+    -10, 00, 00, 00, 00, 00, 00, -10,
+    -10, 00, 00, 00, 00, 00, 00, -10,
+     00, 00, 00, 20, 20, 00, 00,  00,
+};
+
+static int queen[64] = {
+     00, 00, 00, 00, 00, 00, 00,  00,
+    -10, 00, 00, 00, 00, 00, 00, -10,
+    -10, 00, 00, 00, 00, 00, 00, -10,
+    -10, 00, 00, 00, 00, 00, 00, -10,
+    -10, 00, 00, 00, 00, 00, 00, -10,
+    -10, 00, 00, 00, 00, 00, 00, -10,
+    -10, 00, 00, 00, 00, 00, 00, -10,
+     00, 00, 00, 10, 10, 00, 00,  00,
+};
+
+int pieceSquareTable(U64 board, int pieceSquareTable[64], int turn) {
+    int score = 0;
+    while(board) {
+        int square = popLSB(&board);
+        int index = (turn == 1) ? (square) : (63 - square);
+        score += pieceSquareTable[index];
+    }
+    return score;
+}
+
 
 int positionEvaluation(State p) {
     int score = 0;
 
-    // piece value;
-    score += __builtin_popcountll(p.wp) * 1;
-    score += __builtin_popcountll(p.wn) * 3;
-    score += __builtin_popcountll(p.wb) * 3;
-    score += __builtin_popcountll(p.wr) * 5;
-    score += __builtin_popcountll(p.wq) * 9;
+    // piece value
+    // https://gcc.gnu.org/onlinedocs/gcc/Bit-Operation-Builtins.html
+    // more funny things
 
-    score -= __builtin_popcountll(p.bp) * 1;
-    score -= __builtin_popcountll(p.bn) * 3;
-    score -= __builtin_popcountll(p.bb) * 3;
-    score -= __builtin_popcountll(p.br) * 5;
-    score -= __builtin_popcountll(p.bq) * 9;
+    score += __builtin_popcountll(p.wp) * 100 + pieceSquareTable(p.wp, pawn, WHITE);
+    score += __builtin_popcountll(p.wn) * 300 + pieceSquareTable(p.wn, knight, WHITE);
+    score += __builtin_popcountll(p.wb) * 300 + pieceSquareTable(p.wb, bishop, WHITE);
+    score += __builtin_popcountll(p.wr) * 500 + pieceSquareTable(p.wr, rook, WHITE);
+    score += __builtin_popcountll(p.wq) * 900 + pieceSquareTable(p.wq, queen, WHITE);
+    score += pieceSquareTable(p.wk, king, WHITE);
+
+    score -= __builtin_popcountll(p.bp) * 100 + pieceSquareTable(p.bp, pawn, BLACK);
+    score -= __builtin_popcountll(p.bn) * 300 + pieceSquareTable(p.bn, knight, BLACK);
+    score -= __builtin_popcountll(p.bb) * 300 + pieceSquareTable(p.bb, bishop, BLACK);
+    score -= __builtin_popcountll(p.br) * 500 + pieceSquareTable(p.br, rook, BLACK);
+    score -= __builtin_popcountll(p.bq) * 900 + pieceSquareTable(p.bq, queen, BLACK);
+    score -= pieceSquareTable(p.bk, king, BLACK);
 
     return (p.turn == WHITE) ? (score) : (-score);
 }
@@ -54,6 +135,7 @@ int negamax(State p, State prev, int depth, int alpha, int beta) {
     // base case
     if (depth == 0) {
         return positionEvaluation(p);
+        // this should be qsaerch
     }
 
     // get possible moves
@@ -64,10 +146,10 @@ int negamax(State p, State prev, int depth, int alpha, int beta) {
     if (moveCount == 0) {
         if (isInCheck(p)) {
             // being mated is bad
-            return -999999;
+            return -9999999;
         } else {
             // draw is mid
-            return 0;
+            return -150;
         }
     }
 
@@ -81,8 +163,9 @@ int negamax(State p, State prev, int depth, int alpha, int beta) {
         int score = -negamax(temp, p, depth - 1, -beta, -alpha);
        
         // some pruning
+        
         if (score >= beta) {
-            return beta;
+            return score;
         }
 
         // you found a better move. Nice!
@@ -97,10 +180,13 @@ Move negmaxBestMove(State p, State prev, int depth) {
     Move moves[218];
     int moveCount = generateLegalMove(p, prev, moves, 218);
 
-    if (moveCount < 10) {
-        depth = 7;
-    }
+    int numPieces = __builtin_popcountll(allOccupied(p));
 
+    if (numPieces <= 15) {
+        depth = 7;
+    } else if (numPieces < 10){
+        depth = 8;
+    }
 
     Move best = moves[0];
     int alpha = -999999;
