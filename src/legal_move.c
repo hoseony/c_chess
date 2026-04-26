02535943ba595dfd5631865dedaf1a723ee6e791
thomas-george-emp <thomas.george.emp@gmail.com>
2026-04-26 15:00:04 -0400

    bippity boppity

--- src/legal_move.c
+++ src/legal_move.c
@@ -266,6 +266,25 @@ int generateLegalMove(State state, State prevState, Move *moves, int maxMoves, R
     return counter; 
 }
 
+static U64 legalBitboard(State *p, State *prev, int targetSquare, U64 occupied, U64 attackBoard, RookMagic *rookMagic, BishopMagic *bishopMagic) {
+    U64 possibleMoves = generateMoveFromTargetSquare(p, prev, targetSquare, occupied, attackBoard, rookMagic, bishopMagic); 
+    U64 retMoves = possibleMoves; 
+    while (possibleMoves > 0) {
+            int to = popLSB(&possibleMoves);
+
+            State temp = state;
+            doMove(&temp, &prevState, targetSquare, to, false);
+
+            State checkCheck = temp;
+            checkCheck.turn = !checkCheck.turn;
+            if (isInCheck(checkCheck, rookMagic, bishopMagic)) {
+                retMoves ^ (1ULL << to);   
+            }
+    }
+    return retMoves; 
+
+}
+
 
 PerftResult perft(State p, State prev, int depth, RookMagic *rookMagic, BishopMagic *bishopMagic) {
     PerftResult result = {0};
--- src/ui/ui.c
+++ src/ui/ui.c
@@ -1,5 +1,6 @@
 #include "../include/raylib.h"
-#include "../types.h" 
+#include "../bitboards.c" 
+#include "../legal_move.c"
 
 extern int popLSB(U64 *board); 
 
@@ -11,6 +12,10 @@ static const unsigned int gridSquareLength = 80;
 
 static const unsigned int gridStartingPointW = (screenWidth - 8 * gridSquareLength) / 2; 
 static const unsigned int gridStartingPointH = (screenHeight - 8 * gridSquareLength) / 2;
+
+State currentState;
+State prevState; 
+State prevprevState; 
 
 enum piecesTextureID {
     WHITE_PAWN = 0, 
@@ -27,6 +32,9 @@ enum piecesTextureID {
     BLACK_QUEEN = 10,
     BLACK_KING = 11
 };
+
+
+static Texture2D piecesTextures[12]; 
 
 static void drawSquareColors() {
     Color lightSquare = (Color){238, 238, 210, 255};
@@ -45,32 +53,56 @@ inline static int indexGrid(int rank, int file) {
     return rank * 8 + file; 
 }
 
-static void drawGameboard(Texture2D *pieces) {
-    enum piecesTextureID id = WHITE_PAWN; 
-    Rectangle sourceRec = {0.0f, 0.0f, (float)pieces[id].width, (float)pieces[id].height};
-    int piecePosition; 
-    // draw pawn
-    StateUnion tempStateUnion; 
-    tempStateUnion.s = currentState; 
+
+static void drawPieces(State p) {
+    
+
+    StateUnion su;
+
+    Rectangle sourceRec; 
+    Rectangle destRec;
+
+
+    su.s = p; 
+
+
     for (int i = 0; i < 12; i++) {
-        while (tempStateUnion.pieces[i] > 0) {
-            piecePosition = popLSB((tempStateUnion.pieces)+i);
-            DrawTexturePro(pieces[id], sourceRec, (Rectangle){piecePosition / 8, piecePosition % 8, gridSquareLength, 
-                    gridSquareLength}, (Vector2){(float)pieces[id].width, (float)pieces[id].height}, 0.0f, (Color){255, 255, 255, 0}); 
+        for (;su.pieces[i] > 0; ) {
+            int square = popLSB(&su.pieces[i]);
+            // row = square / 8 
+            // col = square % 8 
+            
+            sourceRec = (Rectangle){0.0f, 0.0f, piecesTextures[i].width, piecesTextures[i].height}; 
+            destRec = (Rectangle){gridStartingPointW + (1.6 + (square % 8)) * gridSquareLength, 
+                gridStartingPointH + (1.6 + (7 - (square / 8))) * gridSquareLength,
+            gridSquareLength, gridSquareLength}; 
+            DrawTexturePro(piecesTextures[i], sourceRec, destRec, 
+                    (Vector2){(float)piecesTextures[i].width, (float)piecesTextures[i].height}, 0.0f, (Color){255, 255, 255, 255}); 
         }
     }
 }
 
+inline static U64 squareClicked(Vector2 mousePosition) {
+    U64 boardPosClicked = 1ULL << (((int)mousePosition.x - gridStartingPointW) / gridSquareLength);
+    boardPosClicked <<= 8 * (7 - ((int)mousePosition.y - gridStartingPointH) / gridSquareLength); 
+    return boardPosClicked; 
+}
 
-static Texture2D piecesTextures[12]; 
+inline static void drawBitboard(U64 )
+
 
 int main() {
-    InitWindow(screenWidth, screenHeight, "Chess!"); 
+    
+    InitWindow(screenWidth, screenHeight, "Chess!");
+    Vector2 mousePosition; 
     
     SetTargetFPS(60); 
+
+    currentState = prevState = prevprevState = initializeState();  
         
     // White pieces
     piecesTextures[WHITE_PAWN]   = LoadTexture("../../assets/pieces/white-pawn.png");
+    piecesTextures[WHITE_KNIGHT] = LoadTexture("../../assets/pieces/white-knight.png");
     piecesTextures[WHITE_BISHOP] = LoadTexture("../../assets/pieces/white-bishop.png");
     piecesTextures[WHITE_ROOK]   = LoadTexture("../../assets/pieces/white-rook.png");
     piecesTextures[WHITE_QUEEN]  = LoadTexture("../../assets/pieces/white-queen.png");
@@ -84,20 +116,6 @@ int main() {
     piecesTextures[BLACK_QUEEN]  = LoadTexture("../../assets/pieces/black-queen.png");
     piecesTextures[BLACK_KING]   = LoadTexture("../../assets/pieces/black-king.png");
     
-    int frameWidth = piecesTextures[WHITE_PAWN].width;
-    int frameHeight = piecesTextures[WHITE_PAWN].height;
-
-    // Source rectangle (part of the texture to use for drawing)
-    Rectangle sourceRec = { 0.0f, 0.0f, (float)frameWidth, (float)frameHeight };
-
-    // Destination rectangle (screen rectangle where drawing part of texture)
-    Rectangle destRec = { screenWidth/2.0f, screenHeight/2.0f, frameWidth*2.0f, frameHeight*2.0f };
-
-    // Origin of the texture (rotation/scale point), it's relative to destination rectangle size
-    Vector2 origin = { (float)frameWidth, (float)frameHeight };
-
-    int rotation = 0;
-
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
@@ -107,6 +125,8 @@ int main() {
         // Update
         //----------------------------------------------------------------------------------
         //----------------------------------------------------------------------------------
+        mousePosition = GetMousePosition();
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) printBitboard(squareClicked(mousePosition)); 
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -119,9 +139,8 @@ int main() {
             // destRec defines the rectangle where our texture part will fit (scaling it to fit)
             // origin defines the point of the texture used as reference for rotation and scaling
             // rotation defines the texture rotation (using origin as rotation point)
-            DrawTexturePro(piecesTextures[WHITE_PAWN], sourceRec, destRec, origin, (float)rotation, (Color){255, 255, 255, 255});
             drawSquareColors();
-
+            drawPieces(currentState);
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
