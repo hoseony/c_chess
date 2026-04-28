89277d7c7aa41ad03117679394f40799a89a5214
thomas-george-emp <thomas.george.emp@gmail.com>
2026-04-28 19:56:38 -0400

    here

--- src/legal_move.h
+++ src/legal_move.h
@@ -14,10 +14,13 @@
 
 
 U64 generateMoveFromTargetSquare(State *p, State *prev, int targetSquare, U64 occupied, U64 attackBoard, RookMagic *rookMagic, BishopMagic *bishopMagic);
+U64 legalBitboard(State *p, State *prev, int targetSquare, U64 occupied, U64 attackBoard, RookMagic *rookMagic, BishopMagic *bishopMagic);
 void doMove(State *p, State *prev, int from, int to, bool perftQuestionMark);
 int areYouMated(State *p, State *prev, RookMagic *rookMagic, BishopMagic *bishopMagic, U64 occupied);
 int generateLegalMove(State state, State prevState, Move *moves, int maxMoves, RookMagic *rookMagic, BishopMagic *bishopMagic);
 PerftResult perft(State p, State prev, int depth, RookMagic *rookMagic, BishopMagic *bishopMagic);
 void runPerft(RookMagic *rookMagic, BishopMagic *bishopMagic);
+
+
 
 #endif
